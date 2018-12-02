f3df0df65706ab57d4f15cbd488388ddce764c7e
Stephen Swensen <stephen@imperfectproduce.com>
2018-12-01 19:43:13 -0500

    finished day 1 part a

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let x = day1();
+    println!("Result: {}", x);
+}
+
+fn day1() -> i32 {
+    let txt = fs::read_to_string("day1.txt").unwrap();
+    let lines = txt.split("\n").collect::<Vec<_>>();
+    let sum = lines.iter().map(|x| x.parse::<i32>().unwrap_or(0)).sum();
+    return sum;
 }
