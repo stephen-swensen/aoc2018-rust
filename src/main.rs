68784fbda4c093c4bafb0ea13d929a5ce4637b69
Stephen Swensen <stephen@imperfectproduce.com>
2018-12-01 19:54:39 -0500

    organize inputs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
 }
 
 fn day1() -> i32 {
-    let txt = fs::read_to_string("day1.txt").unwrap();
+    let txt = fs::read_to_string("inputs/day1.txt").unwrap();
     let lines = txt.split("\n").collect::<Vec<_>>();
     let sum = lines.iter().map(|x| x.parse::<i32>().unwrap_or(0)).sum();
     return sum;
