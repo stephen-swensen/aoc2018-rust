a84e42517333ef9d2f2245a1a7fdf3b9ba59888b
Stephen Swensen <stephen@imperfectproduce.com>
2018-12-01 19:57:59 -0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,9 @@ fn main() {
 
 fn day1() -> i32 {
     let txt = fs::read_to_string("inputs/day1.txt").unwrap();
-    let lines = txt.split("\n").collect::<Vec<_>>();
-    let sum = lines.iter().map(|x| x.parse::<i32>().unwrap_or(0)).sum();
+    let sum = txt
+        .split("\n")
+        .map(|x| x.parse::<i32>().unwrap_or(0))
+        .sum();
     return sum;
 }
