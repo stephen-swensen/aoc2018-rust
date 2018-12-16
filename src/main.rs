44013336c575424c04d895d96f6d54bd18779929
Stephen Swensen <stephen@imperfectproduce.com>
2018-12-16 15:29:25 -0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,20 @@ fn day1() -> i32 {
     let txt = fs::read_to_string("inputs/day1.txt").unwrap();
     let sum = txt
         .split("\n")
-        .map(|x| x.parse::<i32>().unwrap_or(0))
+        .filter_map(|x| x.parse::<i32>().ok())
         .sum();
     sum
 }
+
+//fn day1_part2() -> i32 {
+//    let txt = fs::read_to_string("inputs/day1.txt").unwrap();
+//    let frequencies = vec![0];
+//    for ds
+//
+//
+//    let sum = txt
+//        .split("\n")
+//        .map(|x| x.parse::<i32>().unwrap_or(0))
+//        .sum();
+//    sum
+//}
