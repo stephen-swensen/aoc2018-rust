6189317fe78759dc754ef1c18ec3793f041056e9
Stephen Swensen <stephen@imperfectproduce.com>
2018-12-16 15:37:26 -0500

    refactor to use modules

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,27 @@
-use std::fs;
 
 fn main() {
-    let x = day1();
+    let x = day1::part1();
     println!("Result: {}", x);
 }
 
-fn day1() -> i32 {
-    let txt = fs::read_to_string("inputs/day1.txt").unwrap();
-    let sum = txt
-        .split("\n")
-        .filter_map(|x| x.parse::<i32>().ok())
-        .sum();
-    sum
-}
+mod day1 {
+    use std::fs;
 
-//fn day1_part2() -> i32 {
-//    let txt = fs::read_to_string("inputs/day1.txt").unwrap();
-//    let frequencies = vec![0];
-//    for ds
-//
-//
-//    let sum = txt
-//        .split("\n")
-//        .map(|x| x.parse::<i32>().unwrap_or(0))
-//        .sum();
-//    sum
-//}
+    pub fn part1() -> i32 {
+        let txt = fs::read_to_string("inputs/day1.txt").unwrap();
+        let sum = txt
+            .split("\n")
+            .filter_map(|x| x.parse::<i32>().ok())
+            .sum();
+        sum
+    }
+
+    pub fn part2() -> i32 {
+        let txt = fs::read_to_string("inputs/day1.txt").unwrap();
+        let sum = txt
+            .split("\n")
+            .filter_map(|x| x.parse::<i32>().ok())
+            .sum();
+        sum
+    }
+}
