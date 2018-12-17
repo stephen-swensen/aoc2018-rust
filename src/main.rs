bac72bdebab1b06ba2a12883a3e82079f9fc5417
Stephen Swensen <stephen@imperfectproduce.com>
2018-12-17 09:29:08 -0500

    implement day 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 
 fn main() {
-    let x = day1::part1();
-    println!("Result: {}", x);
+    let x = day1::part2().unwrap();
+    println!("Result: {:?}", x);
 }
 
 mod day1 {
     use std::fs;
+    use std::collections::HashSet;
 
     pub fn part1() -> i32 {
         let txt = fs::read_to_string("inputs/day1.txt").unwrap();
@@ -16,12 +17,25 @@ mod day1 {
         sum
     }
 
-    pub fn part2() -> i32 {
+    pub fn part2() -> Option<(usize,i32)> {
         let txt = fs::read_to_string("inputs/day1.txt").unwrap();
-        let sum = txt
+
+        let changes = txt
             .split("\n")
-            .filter_map(|x| x.parse::<i32>().ok())
-            .sum();
-        sum
+            .filter_map(|x| x.parse::<i32>().ok());
+
+        let mut prev = 0;
+        let mut freqs = HashSet::new();
+        freqs.insert(0);
+
+        for (i,x) in changes.cycle().enumerate() {
+            let next = prev + x;
+            if freqs.contains(&next) {
+                return Some((i,next));
+            }
+            prev = next;
+            freqs.insert(next);
+        }
+        None
     }
 }
