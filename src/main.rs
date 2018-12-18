48dcb85a0b5174557ace4f1337cee1c1b76f33b3
Stephen Swensen <stephen@imperfectproduce.com>
2018-12-18 10:20:51 -0500

    add maplit dependency for hashset macro

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 
 fn main() {
     let x = day1::part2().unwrap();
@@ -6,7 +7,7 @@ fn main() {
 
 mod day1 {
     use std::fs;
-    use std::collections::HashSet;
+    use maplit::*;
 
     pub fn part1() -> i32 {
         let txt = fs::read_to_string("inputs/day1.txt").unwrap();
@@ -25,8 +26,7 @@ mod day1 {
             .filter_map(|x| x.parse::<i32>().ok());
 
         let mut prev = 0;
-        let mut freqs = HashSet::new();
-        freqs.insert(0);
+        let mut freqs = hashset![0];
 
         for (i,x) in changes.cycle().enumerate() {
             let next = prev + x;
