38d61c0d48befde9024551369e3c6b98e6c1c6ec
Stephen Swensen <stephen.swensen@gmail.com>
2018-12-20 20:21:43 -0500

    set up for day 4

---
+++ src/day4.rs
@@ -0,0 +1,16 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
+use std::fs;
+use std::str::Lines;
+use maplit::*;
+use std::str::FromStr;
+use std::collections::HashSet;
+
+pub fn part1() -> i32 {
+    0
+}
+
+pub fn part2() -> i32 {
+    0
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
-    let x = day3::part1();
+    let x = day4::part1();
     println!("Result: {:?}", x);
 }
