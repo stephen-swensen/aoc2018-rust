e642466dc276042c14b237c82123af8fb7cb1ac9
Stephen Swensen <stephen.swensen@gmail.com>
2018-12-21 21:09:00 -0500

    implement group_by as a trait extension

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ mod day2;
 mod day3;
 mod day4;
 
-use crate::utils::group_by;
+use crate::utils::VecExt;
 
 fn main() {
     //let x = day4::part1();
     let items = vec![(1,2),(2,1),(3,2),(4,3),(1,3)];
-    let grouped = group_by(items, |&(_,b)| b);
+    let grouped = items.clone().group_by(|&(_,b)| b);
     println!("Result: {:#?}", grouped);
+    for item in items {
+        println!("{:?}", item)
+    }
 }
