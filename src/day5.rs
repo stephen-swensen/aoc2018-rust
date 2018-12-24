4cbd1586b168f8723268b5d4f55a32f70ee5fce3
Stephen Swensen <stephen.swensen@gmail.com>
2018-12-24 17:16:22 -0500

    reimplement using loop - solve day 5 part 1

--- src/day5.rs
+++ src/day5.rs
@@ -10,22 +10,24 @@ use regex::Regex;
 use crate::utils::IteratorExt;
 
 fn react(mut chars: Vec<char>) -> Vec<char> {
-    for (i, c1) in chars.iter().enumerate() {
-        if i == chars.len() - 1 {
-            //println!("Reached end of loop");
-            return chars;
-        } else {
-            let c2 = chars.get(i+1).unwrap();
-            if c1 != c2 && c1.to_ascii_lowercase() == c2.to_ascii_lowercase() {
-                //println!("Removed {} at {} and {} at {}", c1, i, c2, i+1);
-                chars.remove(i);
-                chars.remove(i); //we use i again because of shift left above
-                return react(chars);
+    'outer: loop {
+        'inner: for (i, c1) in chars.iter().enumerate() {
+            if i == chars.len() - 1 {
+                //println!("Reached end of loop");
+                break 'outer;
+            } else {
+                let c2 = chars.get(i+1).unwrap();
+                if c1 != c2 && c1.to_ascii_lowercase() == c2.to_ascii_lowercase() {
+                    //println!("Removed {} at {} and {} at {}", c1, i, c2, i+1);
+                    chars.remove(i);
+                    chars.remove(i); //we use i again because of shift left above
+                    break 'inner;
+                }
             }
         }
     }
     //println!("Exit function");
-    return chars; //this shouldn't be possible
+    return chars;
 }
 
 pub fn part1() -> usize {
