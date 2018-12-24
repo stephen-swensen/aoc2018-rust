2f9c55debd5bc47666d8d95d4d06aa3cc2271cc9
Stephen Swensen <stephen.swensen@gmail.com>
2018-12-24 17:20:13 -0500

    clean up

--- src/day5.rs
+++ src/day5.rs
@@ -1,24 +1,16 @@
 #![allow(dead_code)]
-#![allow(unused_imports)]
+//#![allow(unused_imports)]
 
 use std::fs;
-use std::str::Lines;
-use maplit::*;
-use std::str::FromStr;
-use std::collections::HashMap;
-use regex::Regex;
-use crate::utils::IteratorExt;
 
 fn react(mut chars: Vec<char>) -> Vec<char> {
     'outer: loop {
         'inner: for (i, c1) in chars.iter().enumerate() {
             if i == chars.len() - 1 {
-                //println!("Reached end of loop");
                 break 'outer;
             } else {
                 let c2 = chars.get(i+1).unwrap();
                 if c1 != c2 && c1.to_ascii_lowercase() == c2.to_ascii_lowercase() {
-                    //println!("Removed {} at {} and {} at {}", c1, i, c2, i+1);
                     chars.remove(i);
                     chars.remove(i); //we use i again because of shift left above
                     break 'inner;
@@ -26,7 +18,6 @@ fn react(mut chars: Vec<char>) -> Vec<char> {
             }
         }
     }
-    //println!("Exit function");
     return chars;
 }
 
@@ -34,7 +25,6 @@ pub fn part1() -> usize {
     let input = fs::read_to_string("inputs/day5.txt").unwrap();
     let chars: Vec<char> = input.chars().collect();
     let chars = react(chars);
-    //println!("{:?}", chars);
     chars.len()
 }
 
