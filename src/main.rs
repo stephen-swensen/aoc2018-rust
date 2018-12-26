c70bdd08a646e59941b5f4ccc0564f8699ad134b
Stephen Swensen <stephen.swensen@gmail.com>
2018-12-26 09:15:34 -0500

    a day5c impl - faster than day5 but not as fast as day5b

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,14 @@ mod day4;
 mod day4b;
 mod day5;
 mod day5b;
+mod day5c;
 
 use time::PreciseTime;
 
 fn main() {
     let start = PreciseTime::now();
 
-    let x = day5b::part2();
+    let x = day5::part2();
     //let x: Vec<i32> = vec![1,2,3,4];
     //let x = &x[1..];
     println!("Result: {:#?}", x);
